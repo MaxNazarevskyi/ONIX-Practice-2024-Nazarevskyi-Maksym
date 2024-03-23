68b28ab6f90c8ebfe0c26104ca438027f3b5659c
Maksim Nazarevskyi <ithefirstly@gmail.com>
2024-03-23 22:43:02 +0200

    Added winner definition

--- app/src/main/java/Game.kt
+++ app/src/main/java/Game.kt
@@ -6,7 +6,7 @@ fun main() {
 }
 
 class Game {
-    private val secretNumber = Random.nextInt(0, 101)
+    private val secretNumber = Random.nextInt(1, 100)
     private var currentPlayer: Player = HumanPlayer()
     private var nextPlayer: Player = ComputerPlayer(secretNumber)
 
@@ -21,7 +21,13 @@ class Game {
 
             if (checkGuess(guess)) {
                 println("Congratulations! ${currentPlayer.name()} guessed the number! Number of attempts: ${currentPlayer.attempts}")
-                currentPlayer = nextPlayer.also { nextPlayer = currentPlayer }  // Switching
+                if(currentPlayer.name() == "Bot") {
+                    println("Attempts for ${currentPlayer.name()}: ${currentPlayer.attempts}")
+                    println("Attempts for ${nextPlayer.name()}: ${nextPlayer.attempts}")
+                    println("The winner is ${getWinner()}!")
+                    println("Game over!")
+                }
+                switchPlayers()
             } else {
                 println("Try again")
             }
@@ -42,13 +48,24 @@ class Game {
             else -> true
         }
     }
+
+    private fun switchPlayers() {
+        currentPlayer = nextPlayer.also { nextPlayer = currentPlayer }
+    }
+
+    private fun getWinner(): String? {
+        return if (currentPlayer.attempts < nextPlayer.attempts) {
+            currentPlayer.name()
+        } else {
+            nextPlayer.name()
+        }
+    }
 }
 
 interface Player {
     fun makeGuess(): Int
     fun name(): String?
     fun setName()
-
     var attempts: Int
 }
 
@@ -72,7 +89,7 @@ class HumanPlayer : Player {
 
 class ComputerPlayer(private val secretNumber: Int) : Player {
     override var attempts: Int = 0
-    private var lastGuess = Random.nextInt(0, 101)
+    private var lastGuess = Random.nextInt(1, 100)
 
     override fun makeGuess(): Int {
         val guess = when {
@@ -86,6 +103,7 @@ class ComputerPlayer(private val secretNumber: Int) : Player {
             }
             else -> Random.nextInt(secretNumber - 10, secretNumber + 11)
         }
+        Thread.sleep(750)
         return guess
     }
 
